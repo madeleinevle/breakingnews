3d34eb8c8ce0e2ba60951875942a188fd011ee13
madeleinevle <madeleine.v.le@gmail.com>
2017-12-12 10:34:05 -0800

    Cleaning up code

--- basecode.py
+++ basecode.py
@@ -4,7 +4,7 @@ from urllib import urlencode
 import json, jinja2, os, webapp2
 import logging
 
-top_stories_api_key = "Insert Key Here"
+top_stories_api_key = "4ed4f3c70d424ee0bb3c83e2e95374ef"
 
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
                                         extensions=['jinja2.ext.autoescape'],
@@ -165,7 +165,6 @@ class respondHandler(webapp2.RequestHandler):
             toptenarticles = listofarticles[:10]
             listofclassarticles = []
             for x in toptenarticles:
-                # print(article(x))
                 listofclassarticles.append(article(x))
             values["articles"] = listofclassarticles
 
